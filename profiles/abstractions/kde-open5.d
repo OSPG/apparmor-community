0d8359fb91ba19024a34cef3bc46198d8e72be57
David Roman <droman@ifae.es>
2026-07-06 14:28:01 +0200

    update kde-open5

--- profiles/abstractions/kde-open5.d
+++ profiles/abstractions/kde-open5.d
@@ -1,1 +1,7 @@
+# Gentoo/Plasma 6 URL helper and its normal desktop side effects.
 /usr/bin/kde-open rix,
+/usr/bin/firefox rPx,
+/usr/share/knotifications6/* r,
+owner @{HOME}/.local/share/#* rw,
+owner @{HOME}/.local/share/recently-used.xbel rwk,
+owner @{HOME}/.local/share/recently-used.xbel.lock rwk,
